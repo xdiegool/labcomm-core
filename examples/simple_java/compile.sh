5185e5a4ab631c2a2cc62bb8fb4140ea690b3440
Sven Robertz <sven@cs.lth.se>
2011-11-08 16:29:41 +0100

    use liblabcomm instead in example

--- examples/simple_java/compile.sh
+++ examples/simple_java/compile.sh
@@ -2,7 +2,11 @@ java -jar ../../labComm.jar --java=gen --c=gen/simple.c --h=gen/simple.h  simple
 
 javac -cp ../../lib/java:. gen/*.java Encoder.java Decoder.java
 
-gcc -o example_encoder -I . -I ../../lib/c example_encoder.c gen/simple.c ../../lib/c/labcomm.c ../../lib/c/labcomm_fd_reader_writer.c
+gcc -o example_encoder -L ../../lib/c -I . -I ../../lib/c example_encoder.c gen/simple.c -llabcomm
+gcc -o example_decoder -L ../../lib/c -I . -I ../../lib/c example_decoder.c gen/simple.c -llabcomm
 
-gcc -o example_decoder -I . -I ../../lib/c example_decoder.c gen/simple.c ../../lib/c/labcomm.c ../../lib/c/labcomm_fd_reader_writer.c
+#gcc -o example_encoder -I . -I ../../lib/c example_encoder.c gen/simple.c ../../lib/c/labcomm.c ../../lib/c/labcomm_fd_reader_writer.c
+
+#gcc -o example_decoder -I . -I ../../lib/c example_decoder.c gen/simple.c ../../lib/c/labcomm.c ../../lib/c/labcomm_fd_reader_writer.c
+
 
