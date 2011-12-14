51122380c818ed07ee63d6b1b6af5365e7785d4c
Sven Robertz <sven@cs.lth.se>
2011-12-14 16:44:06 +0100

    ... and updated paths

--- examples/simple/datagram/udp_compile.sh
+++ examples/simple/datagram/udp_compile.sh
@@ -2,6 +2,6 @@
 
 (cd ..; sh compile.sh)
 
-gcc -g -o udp_encoder -L ../../../lib/c -I . -I .. -I ../../../lib/c udp_encoder.c ../gen/simple.c -llabcomm
+gcc -g -o udp_encoder -L ../../../lib/c -I . -I .. -I ../../../lib/c -I ../../../lib/c/experimental udp_encoder.c ../gen/simple.c -llabcomm
 
-gcc -g -o udp_decoder -L ../../../lib/c -I . -I .. -I ../../../lib/c udp_decoder.c ../gen/simple.c -llabcomm
+gcc -g -o udp_decoder -L ../../../lib/c -I . -I .. -I ../../../lib/c -I ../../../lib/c/experimental udp_decoder.c ../gen/simple.c -llabcomm
