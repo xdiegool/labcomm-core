aa7b035794a6e0f7484f3a13293f543b9068af68
Anders Blomdell <anders.blomdell@control.lth.se>
2013-06-03 16:13:29 +0200

    Added nargs variant of ioctl's.

--- lib/c/labcomm_ioctl.h
+++ lib/c/labcomm_ioctl.h
@@ -63,14 +63,23 @@
   LABCOMM_IOC(LABCOMM_IOC_NOSIG,LABCOMM_IOC_NONE,type,nr,0)
 #define LABCOMM_IOR(type,nr,size)					\
   LABCOMM_IOC(LABCOMM_IOC_NOSIG,LABCOMM_IOC_READ,type,nr,sizeof(size))
+/* FIXME: add flag to differentiate between size and nargs */
+#define LABCOMM_IORN(type,nr,nargs)					\
+  LABCOMM_IOC(LABCOMM_IOC_NOSIG,LABCOMM_IOC_READ,type,nr,nargs)
 #define LABCOMM_IOW(type,nr,size)					\
   LABCOMM_IOC(LABCOMM_IOC_NOSIG,LABCOMM_IOC_WRITE,type,nr,sizeof(size))
+#define LABCOMM_IOWN(type,nr,nargs)					\
+  LABCOMM_IOC(LABCOMM_IOC_NOSIG,LABCOMM_IOC_WRITE,type,nr,nargs)
 #define LABCOMM_IOS(type,nr)					\
   LABCOMM_IOC(LABCOMM_IOC_USESIG,LABCOMM_IOC_READ,type,nr,0)
 #define LABCOMM_IOSR(type,nr,size)					\
   LABCOMM_IOC(LABCOMM_IOC_USESIG,LABCOMM_IOC_READ,type,nr,sizeof(size))
+#define LABCOMM_IOSRN(type,nr,nargs)					\
+  LABCOMM_IOC(LABCOMM_IOC_USESIG,LABCOMM_IOC_READ,type,nr,nargs)
 #define LABCOMM_IOSW(type,nr,size)					\
   LABCOMM_IOC(LABCOMM_IOC_USESIG,LABCOMM_IOC_WRITE,type,nr,sizeof(size))
+#define LABCOMM_IOSWN(type,nr,nargs)					\
+  LABCOMM_IOC(LABCOMM_IOC_USESIG,LABCOMM_IOC_WRITE,type,nr,nargs)
 
 struct labcomm_ioctl_register_signature {
   int index;
