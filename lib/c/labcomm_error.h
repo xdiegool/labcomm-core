984ddf795469bfe99682b3fbdfef7452bf35d8ef
Anders Blomdell <anders.blomdell@control.lth.se>
2013-06-28 20:59:29 +0200

    Added GPL license.

--- lib/c/labcomm_error.h
+++ lib/c/labcomm_error.h
@@ -1,3 +1,24 @@
+/*
+  labcomm_error.h -- labcomm error declarations
+
+  Copyright 2013 Anders Blomdell <anders.blomdell@control.lth.se>
+
+  This file is part of LabComm.
+
+  LabComm is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  LabComm is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #ifndef __LABCOMM_ERROR_H__
 #define __LABCOMM_ERROR_H__
 
--- lib/c/labcomm_ioctl.h
+++ lib/c/labcomm_ioctl.h
@@ -1,3 +1,24 @@
+/*
+  labcomm_ioctl.h -- labcomm ioctl declarations
+
+  Copyright 2013 Anders Blomdell <anders.blomdell@control.lth.se>
+
+  This file is part of LabComm.
+
+  LabComm is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  LabComm is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #ifndef __LABCOMM_IOCTL_H__
 #define __LABCOMM_IOCTL_H__
 
