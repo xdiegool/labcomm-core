ba5247576723bb97bc90348d55a5cbb89b52f84d
Tommy Olofsson <tommy.olofsson.90@gmail.com>
2014-04-16 23:42:27 +0200

    In the process of adding a copy function to the generated C code. It compiles now.

--- examples/simple/compile.sh
+++ examples/simple/compile.sh
@@ -5,10 +5,12 @@ java -jar ../../compiler/labComm.jar --java=gen --c=gen/simple.c --h=gen/simple.
 
 javac -cp ../../lib/java:. gen/*.java Encoder.java Decoder.java
 
-gcc -Wall -Werror -I. -I../../lib/c -L../../lib/c \
+gcc -Wall -Werror -Wno-unused-function \
+    -I. -I../../lib/c -L../../lib/c \
     -o example_encoder example_encoder.c gen/simple.c \
     -llabcomm -Tlabcomm.linkscript
-gcc -Wall -Werror -I . -I ../../lib/c -L../../lib/c \
+gcc -Wall -Werror -Wno-unused-function \
+    -I . -I ../../lib/c -L../../lib/c \
     -o example_decoder example_decoder.c gen/simple.c \
     -llabcomm -Tlabcomm.linkscript
 
