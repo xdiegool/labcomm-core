8973c6d7e360de81fa25b08d19029f7f60cbb611
Anders Blomdell <anders.blomdell@control.lth.se>
2014-11-14 13:29:10 +0100

    First implementation of sample references, only for C this far.

--- lib/c/test/test_signature_numbers.c
+++ lib/c/test/test_signature_numbers.c
@@ -5,7 +5,7 @@
 #include "test/gen/generated_encoding.h"
 
 static void info(char *name, char *full_name, 
-		 struct labcomm_signature *signature) {
+		 const struct labcomm_signature *signature) {
   printf("%s %s %p -> %d\n", name,  full_name, signature, 
 	 labcomm_get_local_index(signature));
   if (labcomm_get_local_index(signature) < 0x40) {
@@ -16,8 +16,8 @@ static void info(char *name, char *full_name,
 int main(int argc, char *argv[])
 {
 #define FUNC(name, full_name) \
-  extern struct labcomm_signature labcomm_signature_##full_name; \
-  info( #name, #full_name, &labcomm_signature_##full_name)
+  info( #name, #full_name, labcomm_signature_##full_name)
+
   LABCOMM_FORALL_SAMPLES_generated_encoding(FUNC, ;);
   LABCOMM_FORALL_SAMPLES_another_encoding(FUNC, ;);
   return 0;
